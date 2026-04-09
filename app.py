882be098b86d791ce9a54d8752b279f46d8905f6
SojalG <rathaurdeepak312@gmail.com>
2026-04-09 11:25:25 +0530

    fingers' detection

--- app.py
+++ app.py
@@ -31,6 +31,24 @@ while True:
     if result.multi_hand_landmarks:
         for hand_landmarks in result.multi_hand_landmarks:
 
+            finger_tips = [4, 8, 12, 16, 20]
+            fingers = []
+
+            # Thumb (special case)
+            if hand_landmarks.landmark[4].x < hand_landmarks.landmark[3].x:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+            # Other fingers
+            for tip in finger_tips[1:]:
+                if hand_landmarks.landmark[tip].y < hand_landmarks.landmark[tip - 2].y:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+
+            print("Fingers:", fingers)
+
             for id, lm in enumerate(hand_landmarks.landmark):
                 h, w, c = frame.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
